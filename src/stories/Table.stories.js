f47eee51b2544f66b5b58041fefba1d8a5e9d7b7
unknown <jddzieciol@gmail.com>
2024-04-04 22:31:18 +0200

    first commit

---
+++ src/stories/Table.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Table from '../Table.js'; // Adjust the import path according to your file structure
+
+export default {
+  title: 'Table',
+  component: Table,
+};
+
+const Template = (args) => <Table {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  tags: [
+    { id: 1, name: 'Tag 1', count: 100 },
+    { id: 2, name: 'Tag 2', count: 200 },
+  ],
+  errorMessage: 'Error loading tags',
+  isFetching: false,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tags: [],
+  errorMessage: '',
+  isFetching: true,
+};
+
+export const ErrorState = Template.bind({});
+ErrorState.args = {
+  tags: [],
+  errorMessage: 'Failed to fetch tags',
+  isFetching: false,
+};
